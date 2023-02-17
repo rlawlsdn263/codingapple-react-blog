07a84b1cd99d327b389d6d4b3eb5d03177f4345c
rlawlsdn263 <rlawlsdn263@naver.com>
2023-02-17 20:35:26 +0900

    [Add] map 학습

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./scripts/05.modal";
+import App from "./scripts/06.map";
 // import App from "./App.js";
 import reportWebVitals from "./reportWebVitals";
 
---
+++ src/scripts/06.map.jsx
@@ -0,0 +1,76 @@
+/*eslint-disable*/
+
+import { useState } from "react";
+import "../App.css";
+
+function App() {
+  let [title, setTitle] = useState([
+    "남자 코트 추천",
+    "광주 치킨 맛집",
+    "자바스크립트 독학",
+  ]);
+  let [like, setLike] = useState(0);
+  let [modal, setModal] = useState(false);
+
+  return (
+    <div className="App">
+{/*       <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      <div className="list">
+        <h4>
+          {title[0]}{" "}
+          <span
+            onClick={() => {
+              setLike(like++);
+            }}
+          >
+            좋아요
+          </span>{" "}
+          {like}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4 onClick={()=>{setModal(!modal)}}>{title[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div> */}
+
+      {
+        title.map((item, index)=>{
+          return(
+            <div className="list" key={index}>
+            <h4>{title[index]}{" "}
+            <span
+            onClick={() => {
+              setLike(like++);
+            }}
+          >
+            좋아요
+          </span>{" "}
+          {like}</h4>
+            <p>2월 17일 발행</p>
+          </div>
+          )
+        })
+      }
+    
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+}
+
+export default App;
